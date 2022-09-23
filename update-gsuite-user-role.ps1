07b81ce38c3bdfd4b2eb89ba9278d9b5c1582379
bytespeed <68957067+bytespeed@users.noreply.github.com>
2022-09-23 16:26:04 -0500

    init

---
+++ update-gsuite-user-role.ps1
@@ -0,0 +1,65 @@
+# Parameters
+	#param (
+	#	[parameter(mandatory=$true)][string]$p12keypath,
+	#	[parameter(mandatory=$true)][string]$adminemail,
+	#	[parameter(mandatory=$true)][string]$customerid,
+	#	[parameter(mandatory=$true)][string]$domainname,
+	#	[parameter(mandatory=$true)][string]$serviceaccountclientid,
+	#	[parameter(mandatory=$true)][string]$appemail,
+	#	[parameter(mandatory=$true)][string]$csvfilepath
+	#)
+	$p12keypath = "C:\Temp\myp12file.p12"
+	$adminemail = "myadminemail@email.com"
+	$customerid = "C1234566"
+	$domainname = "mydomain.com"
+	$serviceaccountclientid = Client ID
+	$appemail = "email from service account"
+	$csvfilepath = "C:\Temp\mycsvfilepath.csv"
+	$gs-schemaname = "AppStream_Bytespeed"
+# Modules
+	$allmodules = get-module -listavailable
+	if ($allmodules.name -notcontains "Configuration") {
+		install-module -name 'Configuration' -scope AllUsers -confirm:$false -force
+	} else {
+		import-module "Configuration"
+	}
+	if ($allmodules.name -notcontains "PSGSuite") {
+		install-module -name 'PSGSuite' -scope AllUsers -confirm:$false -allowclobber -force
+	} else {
+		import-module "PSGSuite"
+	}
+# Configuration G-Suite
+	set-psgsuiteconfig -configname "GSuite" -p12keypath $p12keypath -appemail $appemail -adminemail $adminemail -domain $domainname -serviceaccountclientid $serviceaccountclientid -customerid $customerid
+# Schema Modifications
+	$userschemaexists = get-gsuserschema | where {$_.displayname -eq $gs-schemaname}
+	if (-not $userschemaexists) {
+		new-gsuserschema -schemaname $gs-schemaname -fields (add-gsuserschemafield -fieldname "FederationRole" -fieldtype STRING -readaccesstype ADMINS_AND_SELF),(add-gsuserschemafield -fieldname "SessionDuration" -fieldtype STRING -readaccesstype ADMINS_AND_SELF)
+		$userschemaexists = get-gsuserschema | where {$_.displayname -eq $gs-schemaname}
+	}
+	if ($userschemaexists -and (-not (test-path $csvfilepath))) {
+		$allgsuitegroups = get-gsgroup | select Name,Email,FederationRole,SessionDuration
+		$allgsuitegroups | export-csv $csvfilepath -notypeinformation
+		exit
+	}
+	if ($userschemaexists -and (test-path $csvfilepath)) {
+		$csvfileentries = import-csv $csvfilepath | where {$_.FederationRole -ne $null}
+		foreach ($entry in $csvfileentries) {
+			$groupexists = get-gsgroup $entry.email
+			if ($groupexists) {
+				$groupmemberlist = get-gsgroupmemberlist $entry.email #-Role member
+				if ($groupmemberlist) {
+					foreach ($groupmember in $groupmemberlist) {
+						update-gsuser $groupmember.email -confirm:$false -customschemas @{
+							$userschemaexists.schemaname = @{
+								"FederationRole" = $entry.FederationRole
+								"SessionDuration" = $entry.SessionDuration
+							}
+						} |# out-null
+					}
+				}
+				$groupmemberlist = $null
+			}
+			$groupexists = $null
+		}
+	}
+# Version 1.1 1/15/2021
