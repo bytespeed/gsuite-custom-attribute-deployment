73e5de0c3cbaa976d461f36f7b1763da6c13730b
bytespeed <68957067+bytespeed@users.noreply.github.com>
2023-11-27 15:34:29 -0600

    session duration updates

--- gam-attribute-update.ps1
+++ gam-attribute-update.ps1
@@ -1,9 +1,11 @@
 # Update the following variables (Note: schema is case sensitive)
 $group_email = 'appstream@bytespeed.com'
-$schema = 'AppStream_Bytespeed.FederationRole'
+$schema_federation = 'AppStream_Bytespeed.FederationRole'
+$schema_duration = 'AppStream_Bytespeed.SessionDuration'
 $federation_role = 'arn:aws:iam::0000:role/appstream,arn:aws:iam::0000:saml-provider/sso'
+$SessionDuration = '10800'
 
 #Update user command
-gam print group-members group $group_email fields email | gam csv - gam update user '~email' $schema $federation_role
+gam print group-members group $group_email fields email | gam csv - gam update user '~email' $schema_federation $federation_role $schema_duration $SessionDuration
 "`n"
 Read-Host -Prompt "Press Enter to exit"
---
+++ gam-create-attributes.ps1
@@ -0,0 +1,4 @@
+#Create Schema command
+gam create schema AppStream_Bytespeed field FederationRole type string endfield field SessionDuration type string endfield
+"`n"
+Read-Host -Prompt "Press Enter to exit"
